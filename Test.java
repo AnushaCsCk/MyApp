129e739e735fd0b6f4e0f6ce82ea33ca5e5553db
AnushaCsCk <anushacsck@gmail.com>
2020-09-02 17:40:46 +0530

    New Program

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test{
+
+    public static void main(String args[])
+    {
+        pyramidPattern(5); 
+    }
+
+
+  public static void pyramidPattern(int n)
+   {
+       for (int i=0; i<n; i++) {
+       for (int j=n-i; j>1; j--){
+               System.out.print(" ");
+           }
+           for (int j=0; j<=i; j++ ){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+   }
+}
